317e2fb4e75afa8866409c19cbf82ff54d9b9f17
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2021-12-14 16:16:04 -0600

    [ADD]

--- database/seeders/Area.ts
+++ database/seeders/Area.ts
@@ -5,18 +5,22 @@ export default class AreaSeeder extends BaseSeeder {
   public static async run () {
     await Area.createMany([
       {
+        id: 1,
         name: 'Desarrollo',
         email: 'desarrollo@gmail.com',
       },
       {
+        id: 2,
         name: 'Area 2',
         email: 'area2@gmail.com',
       },
       {
+        id: 3,
         name: 'Area 3',
         email: 'area3@gmail.com',
       },
       {
+        id: 4,
         name: 'Area 4',
         email: 'area4@gmail.com',
       },
